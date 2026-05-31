e36a2007edb52f0429943410ebe11e47dca6b5d6
Guru Shankar <guru03@outlook.com>
2026-05-31 17:34:22 +0530

    blogs list are completed

--- src/app/store/app.reducer.ts
+++ src/app/store/app.reducer.ts
@@ -2,9 +2,11 @@ import { ActionReducerMap } from "@ngrx/store";
 import { AppState } from "./app.state";
 import { angularReducer } from "../angular/state/angular.reducer";
 import { routerReducer } from "@ngrx/router-store";
+import { blogReducer } from "../blogs/state/blog.reducer";
 
 
 export const appReducer: ActionReducerMap<AppState> = {
+    router: routerReducer,
     questions: angularReducer,
-    router: routerReducer
+    blogs: blogReducer
 }
--- src/app/store/app.state.ts
+++ src/app/store/app.state.ts
@@ -1,8 +1,10 @@
 import { RouterReducerState } from "@ngrx/router-store";
 import { QuestionsState } from "../models/question.model";
+import { BlogState } from "../models/blog.model";
 
 
 export interface AppState {
     questions: QuestionsState;
+    blogs: BlogState;
     router: RouterReducerState;
 }
