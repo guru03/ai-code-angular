2f097f103f86b0d0b82ce7d3d7f407d72e11d077
Guru Shankar <guru03@outlook.com>
2026-06-01 10:37:00 +0530

    home page created

--- src/app/blogs/blogs.routes.ts
+++ src/app/blogs/blogs.routes.ts
@@ -8,9 +8,10 @@ export const BLOGS_ROUTES: Routes = [
         path: '',
         component: BlogList,
         children: [
-            { path: 'blogs', component: BlogList },
+            // { path: 'blogs', component: BlogList },
+            { path: ':id', component: BlogDetail },
             { path: 'add-blog', component: AddBlog },
-            { path: 'blogs/:id', component: BlogDetail },
+            { path: 'edit/:id', component: AddBlog },
         ]
     },
 ];
